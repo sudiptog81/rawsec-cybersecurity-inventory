c7f98751c7e9cf9f9e3169a63b540ee515faa15e
Alexandre ZANNI <alexandre.zanni@hotmail.fr>
2018-07-19 20:49:58 +0200

    create national security agencies and services category
    - rename files
    - in JSON data:
      + sort items in alphabetical order
      + remove price key
      + add country key
      + put acronym description in description key instead of name key

--- js/tablefilter/resources/national_security_agencies_and_services.js
+++ js/tablefilter/resources/national_security_agencies_and_services.js
@@ -2,20 +2,18 @@ var filtersConfig = Object.assign({}, baseConfig);
 jQuery.extend(filtersConfig, {
     // Filter types
     col_0: 'input',
-    col_1: 'none',
-    col_2: 'input',
-    col_3: 'select',
-    col_4: 'select',
+    col_1: 'select',
+    col_2: 'none',
+    col_3: 'input',
     // columns data types
     col_types: [
         'caseinsensitivestring',
+        'string',
         'none',
-        'string',
-        'string',
         'string',
     ],
     enable_empty_option: true,
     enable_non_empty_option: true,
 });
-var tf = new TableFilter(document.querySelector('#resources-events'), filtersConfig);
+var tf = new TableFilter(document.querySelector('#resources-national-security-agencies-and-services'), filtersConfig);
 tf.init();
